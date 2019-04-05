b4d44c0655d6c0183270a3da9c6661f049e75bfa
Anway Somani <anwaysomani@gmail.com>
2019-04-05 06:29:54 +0530

    Industry products updated set & UI

--- upjau/industry/urls.py
+++ upjau/industry/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'industryProfile/', views.industry_profile, name='industryProfilePage'),
+
+    url(r'availableProduce/', views.industry_produceAvailable, name='industryPorduceAvailable'),
+
+    url(r'buyLand/', views.industry_buyLand, name='buyLand'),
+
+    url(r'produce/', views.industry_produce, name='produceRecord'),
 ]
--- upjau/industry/views.py
+++ upjau/industry/views.py
@@ -9,3 +9,24 @@ def industry_profile(request):
 
     return render(request, 'dashboard/indprofile.html', context)
 
+def industry_produceAvailable(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+
+    return render(request, 'dashboard/available.html', context)
+
+def industry_buyLand(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'dashboard/buyLand.html', context)
+
+def industry_produce(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'dashboard/produce.html', context)
